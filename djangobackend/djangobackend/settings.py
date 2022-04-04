ca7f76afcaff0a89fc6ef93e32f6405c0de2b933
Ewerton12F <ewerton12freitas@gmail.com>
2022-04-04 13:13:01 -0300

    add: django-react integration

--- djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, '')
+            os.path.join(BASE_DIR, 'reactfrontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'reactfrontend/build/static')
+]
